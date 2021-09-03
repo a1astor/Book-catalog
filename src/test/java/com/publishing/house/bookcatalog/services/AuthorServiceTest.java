bf12a4debdf350e0d8150c5bcc2a5dabee4a8c99
Andrey Sergeenko <a.sergeenko@intechcore.com>
2021-09-03 12:28:08 +0300

    Refatoring pom project.

--- src/test/java/com/publishing/house/bookcatalog/services/AuthorServiceTest.java
+++ src/test/java/com/publishing/house/bookcatalog/services/AuthorServiceTest.java
@@ -1,27 +1,16 @@
 //package com.publishing.house.bookcatalog.services;
 //
-//import java.util.List;
 //
-//import org.junit.Rule;
-//import org.testng.annotations.Test;
+//
+//import com.opentable.db.postgres.junit.EmbeddedPostgresRules;
 //
 //@Test
 //public class AuthorServiceTest {
 //
+//    @Rule
+//    public SingleInstancePostgresRule pg = EmbeddedPostgresRules.singleInstance();
+//
 //    public void getAllAuthorsTest() {
-//        try (EmbeddedPostgres pg = EmbeddedPostgres.start();
-//             Connection c = pg.getPostgresDatabase().getConnection()) {
-//
-//            Statement s = c.createStatement();
-//            ResultSet rs = s.executeQuery("SELECT 1");
-//            assertTrue(rs.next());
-//            assertEquals(1, rs.getInt(1));
-//            assertFalse(rs.next());
-//
-//        } catch (Exception e) {
-//            System.out.print(e);
-//            fail();
-//        }
 //    }
 //
 //}
